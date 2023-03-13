869ed3a79aecf8fa7ff45d509dcad8954fbc8386
Chris Mems <christopher.mems@.com>
2023-03-13 12:22:46 -0500

    map over the contacts array

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import ContactCard from './ContactCard';
+import contacts from './data'
 import './styles.css'
 
 function App() {
 return (
   <>
-    <ContactCard
-      avatar="https://via.placeholder.com/150"
-      name="Jenny Han"
-      email="jenny.han@notreal.com"
-      age={25}
-    />
-    
-    <ContactCard
-      avatar="https://via.placeholder.com/150"
-      name="Jason Long"
-      email="jason.long@notreal.com"
-      age={45}
-    />
-    
-    <ContactCard
-      avatar="https://via.placeholder.com/150"
-      name="Peter Pan"
-      email="peter.pan@neverland.com"
-      age={100}
-    />
+    {contacts.map(contact => (
+      <ContactCard
+        avatar="https://via.placeholder.com/150"
+        name={contact.name}
+        email={contact.email}
+        age={contact.age}
+      />
+    ))}
   </>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,7 @@
+const contacts = [
+  { name: "Jenny Han", email: "jenny.han@notreal.com", age: 25 },
+  { name: "Jason Long", email: "jason.long@notreal.com", age: 45 },
+  { name: "Peter Pan", email: "peter.pan@neverland.com", age: 100 }
+];
+
+export default contacts
