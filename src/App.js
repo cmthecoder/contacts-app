dace4c0ad45156b5245aeb11cb6ecda5a8e0d180
Chris Mems <christopher.mems@.com>
2023-03-13 11:46:43 -0500

    create contact card component and import it in app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
+import ContactCard from './ContactCard';
 import './styles.css'
 
 function App() {
-  const message = 'This is my first variable rendered in JSX!'
-
-  const handleClick = () => {
-    alert('you clicked the message!')
-  }
-
-  return (
-    <div>
-      <img src="https://via.placeholder.com/150" alt="profile" />
-      <div className='user-details'></div>
-      <p>Name: Jenny Han</p>
-      <p>Email: Jenny.Han@notreal.com</p>
-      <p>Age: 25</p>
-    </div>
+return (
+  <>
+    <ContactCard />
+  </>
   );
 }
 
---
+++ src/ContactCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ContactCard = () => {
+  return (
+    <div className='contact-card'>
+      <img src="https://via.placeholder.com/150" alt="profile" />
+      <div className='user-details'></div>
+      <p>Name: Jenny Han</p>
+      <p>Email: Jenny.Han@notreal.com</p>
+      <p>Age: 25</p>
+  </div>
+  )
+}
+
+export default ContactCard
