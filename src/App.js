048e6d7a3ea29a28ecb123af4be7c9d66250d710
Chris Mems <christopher.mems@.com>
2023-03-13 17:53:03 -0500

    call api for random contacts

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
+import { useState, useEffect } from 'react';
 import ContactCard from './ContactCard';
-import contacts from './data'
 import './styles.css'
 
 function App() {
+  const [contacts, setContacts] = useState([])
+
+  
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=3")
+      .then(response => response.json())
+        .then(data => {
+          console.log(data)
+          setContacts(data.results)
+    });
+    
+  }, [])
+  
 return (
   <>
     {contacts.map(contact => (
       <ContactCard
-        avatar="https://via.placeholder.com/150"
-        name={contact.name}
+        key={contact.id.value}
+        avatar={contact.picture.large}
+        name={contact.name.first + ' ' + contact.name.last}
         email={contact.email}
-        age={contact.age}
+        age={contact.dob.age}
       />
     ))}
   </>
