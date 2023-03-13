a76a4e09fa517755ef79f349e8fecbb53a6fd682
Chris Mems <christopher.mems@.com>
2023-03-13 11:55:14 -0500

    set state for age

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
 return (
   <>
     <ContactCard />
+    <ContactCard />
+    <ContactCard />
   </>
   );
 }
--- src/ContactCard.jsx
+++ src/ContactCard.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import { useState } from 'react'
 
 const ContactCard = () => {
+  const [showAge, setShowAge] = useState(false)
+
+
   return (
     <div className='contact-card'>
       <img src="https://via.placeholder.com/150" alt="profile" />
       <div className='user-details'></div>
       <p>Name: Jenny Han</p>
       <p>Email: Jenny.Han@notreal.com</p>
-      <p>Age: 25</p>
+      {showAge && <p>Age: 25</p>}
   </div>
   )
 }
